cefbb8dab29694d69bb8cf376595cc3ce88364e5
reenhilm <christian@kajal.se>
2022-06-28 17:01:48 +0200

    first commit

---
+++ starwars.js
@@ -0,0 +1,43 @@
+let btnGet = document.querySelector('#btnGet');
+let InputCharName = document.querySelector('#InputCharName');
+let output = document.querySelector('#output');
+
+function getApi() {    
+    let charName = InputCharName.value;
+    let fullUri = `https://www.swapi.tech/api/people/?name=${charName}`
+    fetch(fullUri)
+     .then(res => res.json())
+     .then(data => {
+        /* console.log(data) */
+        output.innerHTML = '';
+        if(data.result.length > 0)
+        {
+            /* output.innerHTML += `message: ${data.message}\r\n` */
+            output.innerHTML += `found ${data.result.length} characters matching searchcriteria: ${charName}\r\n`
+            for(let i=0;i<data.result.length;i++)
+            {
+                output.innerHTML += `Character result #${i}\r\n`;
+                output.innerHTML += `${data.result[i].properties.name}, ${data.result[i].description}\r\n`
+                output.innerHTML += `properties: ${JSON.stringify(data.result[i].properties, null, 2)}\r\n`
+            }
+        }
+        else
+        {
+            output.innerHTML += `Could not find Star Wars Character with name: ${charName}`
+        }
+     })
+     .catch(err => console.log(err))
+}
+    
+btnGet.addEventListener('click', e => {
+    'use strict'
+    let forms = document.querySelectorAll('.needs-validation');
+    let form = forms[0];
+    e.preventDefault();
+    e.stopPropagation();
+    
+    if(form.checkValidity())
+        getApi();
+
+    form.classList.add('was-validated');
+});
